3aab6a6002717804bd4b08e46d31f55cab35bfe5
shreyas0924 <shreyasp0924@gmail.com>
2024-02-13 23:25:13 +0530

    pushed gh repo to cloudflare

---
+++ src/aws.ts
@@ -0,0 +1,20 @@
+import { S3 } from "aws-sdk";
+import fs from "fs";
+
+const s3 = new S3({
+  accessKeyId: process.env.ACCESS_ID,
+  secretAccessKey: process.env.SECRET_KEY,
+  endpoint: process.env.ENDPOINT,
+});
+
+export const uploadFile = async (fileName: string, localFilePath: string) => {
+  const fileContent = fs.readFileSync(localFilePath);
+  const response = await s3
+    .upload({
+      Body: fileContent,
+      Bucket: "vercel",
+      Key: fileName,
+    })
+    .promise();
+  console.log(response);
+};
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import "dotenv/config";
 import cors from "cors";
 import { generateRandomId } from "./generateRandom";
 import simpleGit from "simple-git";
 import path from "path";
 import { getAllFiles } from "./getAllFiles";
+import { uploadFile } from "./aws";
 const app: Express = express();
 const port = process.env.PORT || 3000;
-dotenv.config();
 
 app.use(cors());
 app.use(express.json());
@@ -19,10 +19,18 @@ app.get("/", (req: Request, res: Response) => {
 app.post("/deploy", async (req: Request, res: Response) => {
   const repoUrl = req.body.repoUrl;
   const id = generateRandomId();
+
   await simpleGit().clone(repoUrl, path.join(__dirname, `output/${id}`));
   console.log(repoUrl);
+
   const files = getAllFiles(path.join(__dirname, `output/${id}`));
   console.log(files);
+
+  //upload to s3
+  files.forEach(async (file) => {
+    await uploadFile(file.slice(__dirname.length + 1), file);
+  });
+
   res.json({
     id: id,
   });
