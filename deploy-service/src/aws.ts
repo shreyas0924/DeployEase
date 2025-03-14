966c91e67047e2b95d949bab93e0ed2f360345a3
shreyas0924 <shreyasp0924@gmail.com>
2025-03-14 19:39:20 +0000

    updated deploy service envs

--- deploy-service/src/aws.ts
+++ deploy-service/src/aws.ts
@@ -3,8 +3,8 @@ import fs from "fs";
 import path from "path";
 import "dotenv/config";
 const s3 = new S3({
-  accessKeyId: process.env.ACCESS_ID,
-  secretAccessKey: process.env.SECRET_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   endpoint: process.env.ENDPOINT,
 });
 
